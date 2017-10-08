e683b57e36a9f31141bbe491d65a770da8ee9a59
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-10-08 18:49:39 +0000

    observeReset test added and working

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,16 +14,23 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+/**
+ * This test generates a cats game which is tested in gameState
+ *	the first test on gameState returns there is a cat game
+ * Then there is a reset and the gameState checks once again, but
+ *	for an invalid, incompleted game
+**/
 TEST(PiezasTest, observeReset)
 {
 	Piezas test;
-	//asserted = test.gameState();
-	/*
-	for(int i=0; i<4; i++) //column itterator
-		test.dropPiece(i);
+	Piece asserted;
+	asserted = Blank;
+	for(int i=0; i<3; i++)
+		for(int j=0; j<4; j++) //column itterator
+			test.dropPiece(j);
 	EXPECT_EQ(asserted, test.gameState());
 	asserted = Invalid;
 	test.reset();
-	*/
-	EXPECT_EQ(true, true);
+	EXPECT_EQ(test.gameState(), asserted);
 }
+
